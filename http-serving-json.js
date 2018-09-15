4fd12e9fbfb59dbc33f6a84f2070e788db81f625
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-09-14 20:04:06 -0500

    HTTP and JSON data part 2

--- http-serving-json.js
+++ http-serving-json.js
@@ -12,10 +12,11 @@ http.createServer((req, serverRes) =>{
 
     if(req.method === 'GET' && req.url === '/posts'){
 
-        https.get(url, (httpResponse) =>{
+        https.get(url, (httpRes) =>{
             httpRes.on('data', data =>{
                 httpRes.setEncoding('UTF-8');
-                console.log(data);
+                // console.log(data);
+                serverRes.write(data);
             });
 
             httpRes.on('end', () =>{
@@ -24,10 +25,10 @@ http.createServer((req, serverRes) =>{
 
             });
         })
-
+    } else {
+        serverRes.writeHead(404, {'Content-Type': 'text/plain'});
+        serverRes.end('404 ERROR, could not find what you are looking for');
     }
-
-
 }).listen(4444);
 
 console.log('Server is listening');
