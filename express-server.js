92ee796e981f9c380d569e6e8e6c5b52f8c14432
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-09-15 18:36:05 -0500

    Routes

--- express-server.js
+++ express-server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
 let app = express();
 
-app.listen(9999);
+const port = process.env.PORT || 9999;
+
+app.get(('/'), (req, res)=>{
+
+    res.send('<h1>Hello<h1>');
+
+});
+
+app.get('/api', (req, res)=>{
+
+    res.json({name: 'Diego'});
+
+})
+
+app.listen(port);
 
 console.log('it`s working');
